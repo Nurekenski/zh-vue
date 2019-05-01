0f0c4ee7a596aaca4c5fea900ba55cc616125b7c
Nurbol <nurbolmaden@gmail.com>
2019-05-01 15:46:30 +0600

    changes server.js

--- server.js
+++ server.js
@@ -19,7 +19,6 @@ const app = express();
 
 app.use('/',serveStatic(path.join(__dirname,'/dist')));
  
-app.use(staticFileMiddleware);
 app.use(history({
   disableDotRule: true,
   verbose: true
@@ -30,7 +29,7 @@ app.get('/', function (req, res) {
   res.render(path.join(__dirname + '/dist/index.html'));
 });
 
-var server = app.listen(process.env.PORT || 8080, function () {
-  var port = server.address().port;
-  console.log("App now running on port", port);
-});
+const port = process.env.PORT || 8080
+app.listen(port);
+
+console.log('Listening on port ' +port);
